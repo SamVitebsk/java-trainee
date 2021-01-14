736dc60ce4fe39e7536557401e6c565936f654c5
Sam-Vi <sam.vitebsk@yandex.ru>
2021-01-14 21:14:43 +0300

    add module 'shop' delete consoleApp add init.sql to resources add 'create DB' and 'use' to init.sql

--- shop/src/main/java/com/andersen/internetShop/config/SecurityConfig.java
+++ shop/src/main/java/com/andersen/internetShop/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.andersen.internetShop.config;
 
-import com.andersen.internetShop.dao.Role;
 import com.andersen.internetShop.dao.UserRepository;
 import com.andersen.internetShop.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
@@ -23,21 +22,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                    .antMatchers("/auth", "/registration").permitAll()
-                    .antMatchers("/*").hasRole(Role.USER.getAuthority())
-            .and()
-                .formLogin()
-                    .usernameParameter("login")
-                    .passwordParameter("password")
-                    .loginPage("/auth")
-                    .loginProcessingUrl("/login")
-                    .successForwardUrl("/main")
-            .and()
-                .logout()
-                    .logoutUrl("/logout")
-                    .logoutSuccessUrl("/auth");
+        http.csrf().disable().authorizeRequests()
+        .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();
+
+//        http.csrf().disable()
+//                .authorizeRequests()
+//                    .antMatchers("/auth", "/registration").permitAll()
+//                    .antMatchers("/*").hasRole(Role.USER.getAuthority())
+//            .and()
+//                .formLogin()
+//                    .usernameParameter("login")
+//                    .passwordParameter("password")
+//                    .loginPage("/auth")
+//                    .loginProcessingUrl("/login")
+//                    .successForwardUrl("/main")
+//            .and()
+//                .logout()
+//                    .logoutUrl("/logout")
+//                    .logoutSuccessUrl("/auth");
     }
 
     @Bean
@@ -48,7 +53,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.parentAuthenticationManager(new ProviderManager(daoAuthenticationProvider()))
+//                .userDetailsService(userDetailsService());
+
         auth.authenticationProvider(daoAuthenticationProvider());
+
+//        auth.jdbcAuthentication().
     }
 
     @Bean
